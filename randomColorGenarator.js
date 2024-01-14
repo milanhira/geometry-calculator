528b0c3af8324e7d7778c319290aa497540b7668
unknown <Milan Hira>
2024-01-14 11:38:11 +0600

    write a random color generator code before gatting all curd section div

---
+++ randomColorGenarator.js
@@ -0,0 +1,7 @@
+//target all card-id div
+const cardId1 = document.getElementById('card-id-1')
+const cardId2 = document.getElementById('card-id-2')
+const cardId3 = document.getElementById('card-id-3')
+const cardId4 = document.getElementById('card-id-4')
+const cardId5 = document.getElementById('card-id-5')
+const cardId6 = document.getElementById('card-id-6')
